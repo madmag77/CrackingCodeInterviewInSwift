c6b04d3ef7bb07930113ad1844645dd74588e8e7
Artem Goncharov <artemswork@mail.ru>
2018-02-07 18:49:09 +0800

    Added BinaryHeap structure

---
+++ CrackingInterview/common/BinaryHeap.swift
@@ -0,0 +1,15 @@
+//
+//  BinaryHeap.swift
+//  CrackingInterview
+//
+//  Created by Artem Goncharov on 30/01/2018.
+//  Copyright © 2018 MadMag. All rights reserved.
+//
+
+import Foundation
+
+class BinaryHeapImpl<T: Comparable> {
+    var root: BinaryNode<T>?
+
+    
+}
